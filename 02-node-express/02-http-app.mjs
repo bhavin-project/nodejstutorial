6da53162047964df534099ae7930ce9a386454df
bhavin-project <bhavin.project@gmail.com>
2024-08-22 18:04:44 +0530

    change-htmlbasics

--- 02-node-express/02-http-app.mjs
+++ 02-node-express/02-http-app.mjs
@@ -19,12 +19,12 @@ const server = http.createServer((req, res) => {
     res.write(homePage);
     res.end();
   }
-  // about page
-  else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>about page</h1>");
-    res.end();
-  }
+  // // about page
+  // else if (url === "/about") {
+  //   res.writeHead(200, { "content-type": "text/html" });
+  //   res.write("<h1>about page</h1>");
+  //   res.end();
+  // }
   // styles
   else if (url === "/styles.css") {
     res.writeHead(200, { "content-type": "text/css" });
