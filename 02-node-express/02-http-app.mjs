5191c19de9c2ee10d2db38af843d7bbce971902f
bhavin-project <bhavin.project@gmail.com>
2024-08-23 17:43:40 +0530

    express-project

--- 02-node-express/02-http-app.mjs
+++ 02-node-express/02-http-app.mjs
@@ -20,11 +20,11 @@ const server = http.createServer((req, res) => {
     res.end();
   }
   // // about page
-  // else if (url === "/about") {
-  //   res.writeHead(200, { "content-type": "text/html" });
-  //   res.write("<h1>about page</h1>");
-  //   res.end();
-  // }
+  else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>about page</h1>");
+    res.end();
+  }
   // styles
   else if (url === "/styles.css") {
     res.writeHead(200, { "content-type": "text/css" });
